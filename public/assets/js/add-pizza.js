9bba9f156e391bdde046f59faef29e9de39e38b8
JonathanKim424 <vizard424@yahoo.com>
2022-06-10 03:03:11 -0600

    added offline storage capability and online push of data

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -63,7 +63,10 @@ const handlePizzaSubmit = event => {
   .then(postResponse => {
     alert('Pizza created successfully!');
     console.log(postResponse);
-  }).catch(err => console.log(err));
+  }).catch(err => {
+    console.log(err);
+    saveRecord(formData);
+  });
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
